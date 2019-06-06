0b91fb833904c6f9f7ab0e2b8305e640641ead06
YamamotoMao35 <nokiroram@gmail.com>
2019-06-06 12:24:57 +0900

    Create boards_controller
    Create boards_controller
    
    boardsコントローラーを作成してトップページに掲示板の名前が表示されるようにした。

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,12 +1,22 @@
 class BoardsController < ApplicationController
   
   def index
+    @boards = Board.board_list
+    @board = Board.new
   end
 
   def new
   end
 
+  # 書き込み
   def create
+    @board = Board.new(params.require(:board).permit(:title))
+    if @board.save
+      redirect_to action: :index
+    else
+      @boards = Board.board_list
+      render :index
+    end
   end
 
 end
--- app/models/board.rb
+++ app/models/board.rb
@@ -1,6 +1,10 @@
 class Board < ApplicationRecord
   validates :title, presence: true, length: {maximum: 60}
 
+  def self.board_list
+    return Board.all.order("created_at ASC")
+  end
+
   has_many :board_classifications
   has_many :categories, through: :board_classifications
 end
