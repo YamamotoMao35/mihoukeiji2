cbb53d5bee91d3e3593eae9126132593459c226a
YamamotoMao35 <nokiroram@gmail.com>
2019-06-06 11:43:13 +0900

    Update board.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,6 @@
 class Board < ApplicationRecord
   validates :title, presence: true, length: {maximum: 60}
+
+  has_many :board_classifications
+  has_many :categories, through: :board_classifications
 end
