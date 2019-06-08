0abd7a072773eb22523e9f96fe92f125095b72bd
YamamotoMao35 <nokiroram@gmail.com>
2019-06-08 21:56:50 +0900

    Update routes.rb
    Update routes.rb
    
    ルーティングを設定した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "boards#index"
-  resources :boards, only: [:index, :new, :create, :show]
+  resources :boards, only: [:index, :new, :create, :show, :update] do
+    resources :messages, only: [:create]
+  end
 end
