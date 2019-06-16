83821d2a4e372eb450e1ac7f534ff18df3aff4c1
YamamotoMao35 <nokiroram@gmail.com>
2019-06-16 19:44:00 +0900

    Update messages_controller.rb
    Update messages_controller.rb
    
    messages_controllerを整えた

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,11 +1,11 @@
 class MessagesController < ApplicationController
   def create
-    message = Message.create(body: message_params[:body], board_id: message_params[:board_id])
+    message = Message.create(message_params)
     redirect_to "/boards/#{message.board_id}"
   end
 
   private
   def message_params
-    params.permit(:body, :board_id)
+    params.permit(:body,:name, :email, :board_id)
   end
 end
