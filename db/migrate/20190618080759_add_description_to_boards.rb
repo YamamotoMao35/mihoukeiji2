7522ef6e8833e4fe02cff3accabdea0697466fc7
YamamotoMao35 <nokiroram@gmail.com>
2019-06-18 18:35:24 +0900

    Add foreign_key
    Add foreign_key
    
    user_idの外部キーを追加した。

--- db/migrate/20190618080759_add_description_to_boards.rb
+++ db/migrate/20190618080759_add_description_to_boards.rb
@@ -1,5 +1,6 @@
 class AddDescriptionToBoards < ActiveRecord::Migration[5.2]
   def change
     add_column :boards, :description, :text
+    add_reference :boards, :user, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,9 @@ ActiveRecord::Schema.define(version: 2019_06_18_080759) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.bigint "user_id"
     t.index ["title"], name: "index_boards_on_title"
+    t.index ["user_id"], name: "index_boards_on_user_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -62,5 +64,6 @@ ActiveRecord::Schema.define(version: 2019_06_18_080759) do
 
   add_foreign_key "board_classifications", "boards"
   add_foreign_key "board_classifications", "categories"
+  add_foreign_key "boards", "users"
   add_foreign_key "messages", "boards"
 end
