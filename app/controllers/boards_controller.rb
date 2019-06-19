85197635320d4f42ace6342f5b15675b66485275
YamamotoMao35 <nokiroram@gmail.com>
2019-06-19 09:53:23 +0900

    Update boards_controller.rb

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,7 +1,8 @@
 class BoardsController < ApplicationController
-  
+  before_action :move_to_index,except: [:index, :show]
+
   def index
-    @boards = Board.paginate(page: params[:page], per_page: 5).order('created_at ASC')
+    @boards = Board.paginate(page: params[:page], per_page: 10).order('created_at ASC')
   end
 
   def new
@@ -12,10 +13,9 @@ class BoardsController < ApplicationController
   def create
     @board = Board.new(board_params)
     if @board.save
-      redirect_to action: :index
+      redirect_to root_path
     else
-      @boards = Board.paginate(page: params[:page], per_page: 5).order('created_at ASC')
-      render :index
+      redirect_to new_board_path
     end
   end
 
@@ -31,7 +31,10 @@ class BoardsController < ApplicationController
 
   private
   def board_params
-    params.permit(:title)
+    params.require(:board).permit(:title, :description, category_ids: []).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
 end
