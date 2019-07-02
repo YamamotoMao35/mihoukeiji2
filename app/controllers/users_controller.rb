83510e46d3abb4a3d60828856a91c27490acbedb
YamamotoMao35 <nokiroram@gmail.com>
2019-07-02 13:33:24 +0900

    Update mypage-sidebar.html.haml
    Update mypage-sidebar.html.haml
    
    マイページに登録したプロフィール情報を表示させた。
    またプロフィールが作られていなければプロフィールが出ないようにした。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @boards = @user.boards.paginate(page: params[:page], per_page: 10).order('created_at ASC')
     @name = current_user.name
+    @profile = current_user.profile
   end
 
   def edit
