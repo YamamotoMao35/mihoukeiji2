6346c1126931c53fc2a5536a4e80f37441c2cbaa
YamamotoMao35 <nokiroram@gmail.com>
2019-07-02 15:39:07 +0900

    Update profile_alert.js

--- app/assets/javascripts/profile_alert.js
+++ app/assets/javascripts/profile_alert.js
@@ -1,5 +1,5 @@
 $(function(){
-  var text_max = 120; // 最大入力値
+  var text_max = 150; // 最大入力値
   $(".count").text(text_max - $("#self_introduction").val().length);
 
   $("#self_introduction").on("keydown keyup keypress change",function(){
@@ -21,7 +21,7 @@ $(function(){
   $("#profile-submit").click(function(){
     var self_introduction = $("textarea[name='profile[self_introduction]']").val()
     if(self_introduction.length > 120){
-      alert("120文字以内で再記入お願いします");
+      alert("150文字以内で再記入お願いします");
     }
   })
 })
