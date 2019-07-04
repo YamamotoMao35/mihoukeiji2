6496dbbd8d1cfbefd2b82a89a1b118692036e271
YamamotoMao35 <nokiroram@gmail.com>
2019-07-04 14:49:26 +0900

    Update production.rb
    Update production.rb
    
    production.rbにherokuにデプロイする際に必要なコードを入れた

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,11 +23,11 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
-  config.active_storage.service = :local
+  config.active_storage.service = :google
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
@@ -94,4 +94,6 @@ Rails.application.configure do
 
   host = 'samplehost'
   Rails.application.routes.default_url_options[:host] = host
+
+  config.assets.initialize_on_precompile=false
 end
