e5e2c17b6c76a3f90fca48d77924f20afc2602d9
YamamotoMao35 <nokiroram@gmail.com>
2019-07-04 12:45:20 +0900

    Update profile_alert.js
    Update profile_alert.js
    
    他のページでも入力フォームに条件をつけてアラートを入れた。

--- app/assets/javascripts/profile_alert.js
+++ app/assets/javascripts/profile_alert.js
@@ -22,9 +22,33 @@ $(function(){
     });
     $("#profile-submit").click(function(){
       var self_introduction = $("textarea[name='profile[self_introduction]']").val()
-      if(self_introduction.length > 120){
+      if(self_introduction.length > 150){
         alert("150文字以内で再記入お願いします");
       }
     })
+  } else if (pageUrl.match(/\/boards\/\d/)){
+    $(".respond-btn").click(function(){
+      var message_body = $("textarea[name='body']").val();
+      if(message_body.length > 1000){
+        alert("1000文字以内で再記入お願いします");
+      }
+    })
+  } else if (pageUrl == "/boards/new"){
+    $(".thread-new-btn").click(function(){
+      var thread_title = $("input[name='board[title]']").val();
+      var thread_description = $("textarea[name='board[description]']").val();
+      console.log(thread_title)
+      if (thread_title.length > 60){
+        alert("60文字以内で再記入お願いします");
+      }
+      else if (thread_description.length > 1000){
+        alert("1000文字以内で再記入お願いします");
+      }
+      else{
+        console.log("OK");
+      }
+    })
+  }
+  else{
   }
 })
