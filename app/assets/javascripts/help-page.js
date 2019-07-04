663166e7a5b7262908d7812850e9161937f8cb8f
YamamotoMao35 <nokiroram@gmail.com>
2019-07-04 16:14:46 +0900

    Update help-page.js
    Update help-page.js
    
    helpページのみjsが実行されるようにすることでエラーを取り除いた

--- app/assets/javascripts/help-page.js
+++ app/assets/javascripts/help-page.js
@@ -1,10 +1,13 @@
 $(function(){
-  $('#toc').toc({
-    'selectors': '.help-page h2, .help-page h3, .help-page h4, .help-page h5, .help-page h6', //目次として表示する要素のCSSセレクターを指定
-    'highlightOffset': 0,
-    'highlightOnScroll': true,
-    'anchorName': function(i, heading, prefix) {
-      return prefix+i;
-    }
-  });
+  var pageUrl = $(location).attr('pathname');
+  if (pageUrl == "/attention/assistance"){
+    $('#toc').toc({
+      'selectors': '.help-page h2, .help-page h3, .help-page h4, .help-page h5, .help-page h6', //目次として表示する要素のCSSセレクターを指定
+      'highlightOffset': 0,
+      'highlightOnScroll': true,
+      'anchorName': function(i, heading, prefix) {
+        return prefix+i;
+      }
+    });
+  }
 });
