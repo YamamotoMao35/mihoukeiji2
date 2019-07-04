e8d3af63a93268e82c70a0a2f778f3086bcfd803
YamamotoMao35 <nokiroram@gmail.com>
2019-07-04 12:13:36 +0900

    Update profile_alert
    Update profile_alert
    
    アラートが出るページを指定した

--- app/assets/javascripts/profile_alert.js
+++ app/assets/javascripts/profile_alert.js
@@ -1,27 +1,30 @@
 $(function(){
-  var text_max = 150; // 最大入力値
-  $(".count").text(text_max - $("#self_introduction").val().length);
+  var pageUrl = $(location).attr('pathname');
+  if (pageUrl == "/users/1/profiles"){
+    var text_max = 150; // 最大入力値
+    $(".count").text(text_max - $("#self_introduction").val().length);
 
-  $("#self_introduction").on("keydown keyup keypress change",function(){
-    var text_length = $(this).val().length;
-    var countdown = text_max - text_length;
-    $(".count").text(countdown);
-    if(countdown < 0){
-      $('.count').css({
-          color:'#e72510',
-          fontWeight:'bold'
-      });
-    } else {
-      $('.count').css({
-          color:'#000000',
-          fontWeight:'normal'
-      });
-    }
-  });
-  $("#profile-submit").click(function(){
-    var self_introduction = $("textarea[name='profile[self_introduction]']").val()
-    if(self_introduction.length > 120){
-      alert("150文字以内で再記入お願いします");
-    }
-  })
+    $("#self_introduction").on("keydown keyup keypress change",function(){
+      var text_length = $(this).val().length;
+      var countdown = text_max - text_length;
+      $(".count").text(countdown);
+      if(countdown < 0){
+        $('.count').css({
+            color:'#e72510',
+            fontWeight:'bold'
+        });
+      } else {
+        $('.count').css({
+            color:'#000000',
+            fontWeight:'normal'
+        });
+      }
+    });
+    $("#profile-submit").click(function(){
+      var self_introduction = $("textarea[name='profile[self_introduction]']").val()
+      if(self_introduction.length > 120){
+        alert("150文字以内で再記入お願いします");
+      }
+    })
+  }
 })
