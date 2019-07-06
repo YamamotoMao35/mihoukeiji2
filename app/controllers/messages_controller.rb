6dc80571f41960ebc2088b15b23acc45463f14f5
YamamotoMao35 <nokiroram@gmail.com>
2019-07-06 23:02:45 +0900

    Update messages_controller.rb
    Update messages_controller.rb
    
    ユーザーがログインしている時、user_idをmessagesコントローラーに保存できるようにした。

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,15 @@
 class MessagesController < ApplicationController
   def create
     message = Message.new(message_params)
-    if message.save
-      redirect_to "/boards/#{message.board_id}", notice: "レスを投稿しました"
+    if message.save!
+      if user_signed_in?
+        user_id = current_user.id
+        message_user = Message.find(message.id)
+        message_user.update(user_id: user_id)
+        redirect_to "/boards/#{message.board_id}", notice: "レスを投稿しました"
+      else
+        redirect_to "/boards/#{message.board_id}", notice: "レスを投稿しました"
+      end
     else
       redirect_to "/boards/#{message.board_id}", alert: "レスを投稿できませんでした。"
     end
